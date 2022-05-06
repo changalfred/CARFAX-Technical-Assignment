b66c45fc2cec77b20b1150d19ea0e9a1eb0e06ab
Alfred Chang <alfredchang1092@gmail.com>
2022-05-06 02:01:11 -0700

    Add fragment to host acitivty

--- app/src/main/java/com/example/carfax_technical_assignment/MainActivity.kt
+++ app/src/main/java/com/example/carfax_technical_assignment/MainActivity.kt
@@ -7,7 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.main_container, MainFragment())
+                .commit()
+        }
     }
 
-    // TODO: Add main fragment and detail fragment.
+
 }
--- app/src/main/java/com/example/carfax_technical_assignment/MainFragment.kt
+++ app/src/main/java/com/example/carfax_technical_assignment/MainFragment.kt
@@ -1,4 +1,17 @@
 package com.example.carfax_technical_assignment
 
-class MainFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                     savedInstanceState: Bundle?): View? {
+        activity?.setTitle(R.string.company_name)
+
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
 }
